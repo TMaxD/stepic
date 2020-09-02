dd1ac6daa830dff9305c9c009dd9acaead90d125
TMaxD <70047960+TMaxD@users.noreply.github.com>
2020-09-02 06:59:31 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
-sudo ln -sf /home/box/etc/nginx.conf
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo unlink /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
